97a350fb0658b606cdf02f2b8c8e9389096963aa
crescentphases <crescentphases@gmail.com>
2023-06-09 03:14:44 +0200

    first commit, made header, visuals, todo input + button, some glow effects :)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './scss/app.scss'
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div className="container">
+        <Header/>
+      <div className="fill__block">
+        <input type="text" id="todo-text" placeholder="Create a new todo..." />
+        <button className="fill__block-button">GO</button>
+      </div>
+
     </div>
+  </div>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+	return(
+		<header className="header">
+			<div className="header__logo">TO-DO APP</div>
+			<a href="https://github.com/crescentphases" className={'credentials'} target="_blank">made by crescentphases v1.0</a>
+		</header>
+	)
+}
+
+export default Header;
