a4fb3148a47dd0b8767a700bcbe0367cc497fa33
crescentphases <crescentphases@gmail.com>
2023-06-09 05:01:39 +0200

    json + visuals, text-decorations and done , etc.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@ import './scss/app.scss'
 import Header from "./components/Header";
 import ToDoBlock from "./components/ToDoBlock";
 import dataTODOS from './components/json/data.json'
+import React from 'react'
 function App() {
+  const [todos, setTodos] = React.useState(dataTODOS);
+  const [inputValue, setInputValue] = React.useState('');
+
+  React.useEffect(() => {
+    setTodos(dataTODOS);
+  }, [dataTODOS]);
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const addNewToDo = () => {
+    const newTodo = {
+      "id": todos.length,
+      "value": inputValue
+    }
+    setTodos([...todos, newTodo])
+    console.log(todos)
+  };
+
   return (
   <div>
     <div className="container">
         <Header/>
       <div className="fill__block">
-        <input type="text" id="todo-text" placeholder="Create a new todo..." />
-        <button className="fill__block-button">GO</button>
+        <input type="text" value={inputValue} onChange={handleChange} id="todo-text" placeholder="Create a new todo..." />
+        <button onClick={addNewToDo} className="fill__block-button">GO</button>
       </div>
-      {dataTODOS.map(item => <ToDoBlock {...item}/>)}
+      {todos.map(item => <ToDoBlock key={item.id} {...item}/>)}
     </div>
   </div>
   );
--- src/components/ToDoBlock.jsx
+++ src/components/ToDoBlock.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 function ToDoBlock(props) {
 	const {value, isDone} = props;
+	const [doneState, setDoneState] = React.useState(false);
+	console.log(doneState);
 	return(
 		<div className="to__doBlock">
 			<div className="to__doBlock-block">
-				<button className={isDone ? "to__doBlock-block__icon " +
+				<button onClick={() => setDoneState(!doneState)} className={isDone || doneState ? "to__doBlock-block__icon " +
 																"to__doBlock-block__icon-done" : "to__doBlock-block__icon"}>
-					<span></span>
 				</button>
-				<div className={isDone ? "to__doBlock-block__text-done" :
+				<div className={isDone || doneState ? "to__doBlock-block__text-done" :
 					"to__doBlock-block__text"}>
 					{value}
 				</div>
