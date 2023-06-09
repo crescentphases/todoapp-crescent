936432e21d5e7cf48b5b3fa8c8244134dcf8313f
crescentphases <crescentphases@gmail.com>
2023-06-09 03:53:56 +0200

    made todo block, visuals, borders => ready for json and component dev

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './scss/app.scss'
 import Header from "./components/Header";
+
 function App() {
   return (
   <div>
@@ -9,7 +10,17 @@ function App() {
         <input type="text" id="todo-text" placeholder="Create a new todo..." />
         <button className="fill__block-button">GO</button>
       </div>
+      <div className="to__doBlock">
+        <div className="to__doBlock-block">
+          <button className="to__doBlock-block__icon">
+            <span></span>
+          </button>
+          <div className="to__doBlock-block__text">
+            sdagfasdfgsagsa
+          </div>
+        </div>
 
+      </div>
     </div>
   </div>
   );
